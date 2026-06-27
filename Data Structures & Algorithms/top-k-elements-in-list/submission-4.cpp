deea626f65856269177b2c28536845713aff5c90
Anne Christiono <anchris@mit.edu>
2026-06-27 00:56:33 -0700

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        unordered_map<int, int> freq;
+        for(int i : nums) {
+            freq[i] += 1;
+        }
+        vector<vector<int>> counts(nums.size()+1);
+        for(auto& p : freq) {
+            counts[p.second].push_back(p.first);
+        }
+
+        vector<int> ans;
+        for(int i = nums.size(); i >= 0; i--) {
+            ans.insert(ans.end(), counts[i].begin(), counts[i].end());
+            if(ans.size() == k) return ans;
+        }
+    }
+};
